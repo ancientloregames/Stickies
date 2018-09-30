46feb79d1298e6d18deebc9ea53829ec5f160d1a
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-09-30 11:51:39 +0200

    implemented a note addition functionality

--- app/src/main/java/com/ancientlore/stickies/ui/NoteActivity.kt
+++ app/src/main/java/com/ancientlore/stickies/ui/NoteActivity.kt
@@ -1,11 +1,17 @@
 package com.ancientlore.stickies.ui
 
+import android.app.Activity
+import android.content.Intent
 import com.ancientlore.stickies.BR
 import com.ancientlore.stickies.R
 import com.ancientlore.stickies.databinding.ActivityNoteBinding
 import com.ancientlore.stickies.viewmodel.NoteActivityViewModel
 
 class NoteActivity : BasicActivity<ActivityNoteBinding, NoteActivityViewModel>() {
+
+	companion object {
+		const val EXTRA_NOTE_ID = "extra_note_id"
+	}
 
 	override fun getLayoutId() = R.layout.activity_note
 
@@ -14,4 +20,20 @@ class NoteActivity : BasicActivity<ActivityNoteBinding, NoteActivityViewModel>()
 	override fun createViewModel() = NoteActivityViewModel(application)
 
 	override fun getTitleId() = R.string.new_note
+
+	override fun setupViewModel() {
+		super.setupViewModel()
+
+		viewModel.onNoteAdded()
+				.takeUntil(destroyEvent)
+				.subscribe { finishWithResult(it) }
+	}
+
+	private fun finishWithResult(newNoteId: Long) {
+		val intent = Intent().apply {
+			putExtra(EXTRA_NOTE_ID, newNoteId)
+		}
+		setResult(Activity.RESULT_OK, intent)
+		finish()
+	}
 }
--- app/src/main/java/com/ancientlore/stickies/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/ancientlore/stickies/viewmodel/MainActivityViewModel.kt
@@ -1,11 +1,13 @@
 package com.ancientlore.stickies.viewmodel
 
+import android.app.Activity
 import android.app.Application
 import android.content.Intent
 import android.util.Log
 import com.ancientlore.stickies.MutableAdapter
 import com.ancientlore.stickies.data.model.Note
 import com.ancientlore.stickies.data.source.DataSource
+import com.ancientlore.stickies.ui.NoteActivity
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
 
@@ -21,24 +23,47 @@ class MainActivityViewModel(application: Application,
 
 	private val addNoteEvent = PublishSubject.create<Int>()
 
-	init {
-		loadNotes()
+	init { loadNotes() }
+
+	override fun handleActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+		when (requestCode) {
+			INTENT_ADD_NOTE -> handleNoteAdditionResult(resultCode, data)
+		}
+	}
+
+	private fun handleNoteAdditionResult(resultCode: Int, data: Intent?) {
+		when (resultCode) {
+			Activity.RESULT_OK -> getIdAndLoadNote(data)
+		}
+	}
+
+	private fun getIdAndLoadNote(data: Intent?) {
+		data?.run {
+			loadNote(getLongExtra(NoteActivity.EXTRA_NOTE_ID, 0))
+		} ?: Log.w(TAG, "No note id in the NoteActivity data, finished with Success!")
 	}
 
 	private fun loadNotes() {
 		repository.getAll(object : DataSource.ListLoadedCallback<Note> {
-			override fun onSuccess(data: List<Note>) {
-				listAdapter.setItems(data)
-			}
-
+			override fun onSuccess(data: List<Note>) = setListItems(data)
 			override fun onFailure(error: Throwable) {
 				Log.w(TAG, error.message ?: "Some error occurred during the list loading")
 			}
 		})
 	}
 
-	override fun handleActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+	private fun loadNote(id: Long) {
+		repository.getItem(id, object : DataSource.ItemLoadedCallback<Note> {
+			override fun onSuccess(data: Note) = addListItem(data)
+			override fun onFailure(error: Throwable) {
+				Log.w(TAG, error.message ?: "Some error occurred during the loading of an item with id $id")
+			}
+		})
 	}
+
+	private fun setListItems(items: List<Note>) = runOnUiThread(Runnable { listAdapter.setItems(items) })
+
+	private fun addListItem(item: Note) = runOnUiThread(Runnable { listAdapter.addItem(item) })
 
 	fun onAddNoteClicked() = addNoteEvent.onNext(INTENT_ADD_NOTE)
 
