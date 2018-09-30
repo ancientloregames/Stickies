6cf9ddee0939fed270672e24229e0117b1dc238c
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-09-30 10:31:34 +0200

    made the note model immutable

--- app/src/main/java/com/ancientlore/stickies/data/model/Note.kt
+++ app/src/main/java/com/ancientlore/stickies/data/model/Note.kt
@@ -5,6 +5,6 @@ import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 
 @Entity(tableName = "notes")
-data class Note(@PrimaryKey(autoGenerate = true) var id: Long = 0,
-				@field:ColumnInfo var title: String = "",
-				@field:ColumnInfo var body: String = "")
+data class Note(@PrimaryKey(autoGenerate = true) val id: Long,
+				@field:ColumnInfo val title: String,
+				@field:ColumnInfo val body: String)
