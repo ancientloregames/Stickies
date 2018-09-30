d66cebefab7731a47d9d852e40e46ce56fabfe13
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-09-30 14:52:26 +0200

    implemented the note details showing

--- app/src/main/java/com/ancientlore/stickies/notedetail/NoteDetailActivity.kt
+++ app/src/main/java/com/ancientlore/stickies/notedetail/NoteDetailActivity.kt
@@ -7,11 +7,19 @@ import com.android.databinding.library.baseAdapters.BR
 
 class NoteDetailActivity: BasicActivity<ActivityNotedetailBinding, NoteDetailViewModel>() {
 
+	companion object {
+		const val EXTRA_NOTE_ID = "extra_note_id"
+	}
+
 	override fun getLayoutId() = R.layout.activity_notedetail
 
 	override fun getBindingVariable() = BR.viewModel
 
-	override fun createViewModel() = NoteDetailViewModel(application)
+	override fun createViewModel() = NoteDetailViewModel(application, getNoteId())
 
 	override fun getTitleId() = R.string.note_details
+
+	private fun getNoteId() = intent.getLongExtra(EXTRA_NOTE_ID, 0)
+			.takeIf { it != 0L }
+			?: throw RuntimeException("No id was passed to the NoteDetailActivity. It is mandatory!")
 }
--- app/src/main/java/com/ancientlore/stickies/notedetail/NoteDetailViewModel.kt
+++ app/src/main/java/com/ancientlore/stickies/notedetail/NoteDetailViewModel.kt
@@ -1,6 +1,34 @@
 package com.ancientlore.stickies.notedetail
 
 import android.app.Application
+import android.databinding.ObservableField
+import android.util.Log
+import com.ancientlore.stickies.data.model.Note
+import com.ancientlore.stickies.data.source.DataSource
 import com.ancientlore.stickies.viewmodel.BasicViewModel
 
-class NoteDetailViewModel(application: Application): BasicViewModel(application)
+class NoteDetailViewModel(application: Application, noteId: Long): BasicViewModel(application) {
+
+	companion object {
+		private const val TAG = "NoteDetailViewModel"
+	}
+
+	val titleField = ObservableField<String>("")
+	val messageField = ObservableField<String>("")
+
+	init { loadNote(noteId) }
+
+	private fun loadNote(id: Long) {
+		repository.getItem(id, object : DataSource.ItemLoadedCallback<Note> {
+			override fun onSuccess(data: Note) = bind(data)
+			override fun onFailure(error: Throwable) {
+				Log.w(TAG, error.message ?: "Some error occurred during the loading of a note with id $id")
+			}
+		})
+	}
+
+	private fun bind(note: Note) {
+		titleField.set(note.title)
+		messageField.set(note.body)
+	}
+}
