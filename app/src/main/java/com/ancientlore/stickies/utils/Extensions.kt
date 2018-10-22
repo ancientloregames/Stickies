bb477c0941221d12dde89d30ddd78d08c81738aa
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-10-22 19:02:13 +0200

    added context extensions for the convenient work with the keyboard

---
+++ app/src/main/java/com/ancientlore/stickies/utils/Extensions.kt
@@ -0,0 +1,15 @@
+package com.ancientlore.stickies.utils
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+fun Context.showKeybouard(view: View) = showKeybouard(view, 0)
+
+fun Context.showKeybouard(view: View, flags: Int) = view.requestFocus() &&
+		(getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).showSoftInput(view, flags)
+
+fun Context.hideKeyboard(view: View) = hideKeyboard(view, 0)
+
+fun Context.hideKeyboard(view: View, flags: Int) =
+		(getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(view.windowToken, flags)
