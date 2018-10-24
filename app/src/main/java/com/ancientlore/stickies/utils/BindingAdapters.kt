81eef63acc7a6578de9c3d102e2f57946b1cfd7e
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-10-24 19:22:44 +0200

    added a new custom BindingAdapter - srcCompat with a Drawable

--- app/src/main/java/com/ancientlore/stickies/utils/BindingAdapters.kt
+++ app/src/main/java/com/ancientlore/stickies/utils/BindingAdapters.kt
@@ -1,12 +1,19 @@
 package com.ancientlore.stickies.utils
 
 import android.databinding.BindingAdapter
+import android.graphics.drawable.Drawable
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.widget.AppCompatImageView
 import android.support.v7.widget.CardView
 
 @BindingAdapter("srcCompat")
 fun setImageResource(imageView: AppCompatImageView, res: Int) {
 	imageView.setImageResource(res)
+}
+
+@BindingAdapter("srcCompat")
+fun setImageResource(fab: FloatingActionButton, drawable: Drawable) {
+	fab.setImageDrawable(drawable)
 }
 
 @BindingAdapter("cardBackgroundColor")
