7f5717681d68244f7a6dd996fcff2e43db7d2b46
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-10-29 17:53:45 +0200

    added bindings to set compat textview's drawables via the databinding

--- app/src/main/java/com/ancientlore/stickies/utils/BindingAdapters.kt
+++ app/src/main/java/com/ancientlore/stickies/utils/BindingAdapters.kt
@@ -5,6 +5,7 @@ import android.graphics.drawable.Drawable
 import android.support.design.widget.FloatingActionButton
 import android.support.v7.widget.AppCompatImageView
 import android.support.v7.widget.CardView
+import com.ancientlore.stickies.view.DrawableCompatTextView
 
 @BindingAdapter("srcCompat")
 fun setImageResource(imageView: AppCompatImageView, res: Int) {
@@ -20,3 +21,12 @@ fun setImageResource(fab: FloatingActionButton, drawable: Drawable) {
 fun setBackgroundColor(imageView: CardView, color: Int) {
 	imageView.setCardBackgroundColor(color)
 }
+
+@BindingAdapter("drawableStart")
+fun setDrawableStart(imageView: DrawableCompatTextView, drawable: Drawable) = imageView.setDrawableStart(drawable)
+@BindingAdapter("drawableTop")
+fun setDrawableTop(imageView: DrawableCompatTextView, drawable: Drawable) = imageView.setDrawableTop(drawable)
+@BindingAdapter("drawableEnd")
+fun setDrawableEnd(imageView: DrawableCompatTextView, drawable: Drawable) = imageView.setDrawableEnd(drawable)
+@BindingAdapter("drawableBottom")
+fun setDrawableBottom(imageView: DrawableCompatTextView, drawable: Drawable) = imageView.setDrawableBottom(drawable)
