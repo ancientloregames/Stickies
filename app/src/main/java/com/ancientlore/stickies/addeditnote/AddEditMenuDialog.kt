44ac5da5a5fc4c0b44acef50c1d8b883937b5fed
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-11-09 19:21:28 +0200

    fixed the note creation menu string resources

--- app/src/main/java/com/ancientlore/stickies/addeditnote/AddEditMenuDialog.kt
+++ app/src/main/java/com/ancientlore/stickies/addeditnote/AddEditMenuDialog.kt
@@ -22,21 +22,21 @@ internal class AddEditMenuDialog: BottomMenuDialog() {
 			add(getImportanceMenuItem(currentState.isImportant))
 			add(getCompletedMenuItem(currentState.isCompleted))
 			add(MenuItem(R.id.im_colorpicker, R.string.menu_colorpicker, R.drawable.ic_colorpicker))
-			add(MenuItem(R.id.im_alarm, R.string.menu_schedule_alarm, R.drawable.ic_alarm))
+			add(MenuItem(R.id.im_alarm, R.string.menu_schedule_reminder, R.drawable.ic_alarm))
 		}
 	}
 
 	private fun getImportanceMenuItem(isImportant: Boolean): MenuItem {
 		return when (isImportant) {
-			true -> MenuItem(R.id.im_important, R.string.menu_unimportant, R.drawable.ic_important_off)
-			else -> MenuItem(R.id.im_important, R.string.menu_important, R.drawable.ic_important_on)
+			true -> MenuItem(R.id.im_important, R.string.menu_unimportant_full, R.drawable.ic_important_off)
+			else -> MenuItem(R.id.im_important, R.string.menu_important_full, R.drawable.ic_important_on)
 		}
 	}
 
 	private fun getCompletedMenuItem(isCompleted: Boolean): MenuItem {
 		return when (isCompleted) {
-			true -> MenuItem(R.id.im_completed, R.string.menu_uncompleted, R.drawable.ic_completed_off)
-			else -> MenuItem(R.id.im_completed, R.string.menu_completed, R.drawable.ic_completed_on)
+			true -> MenuItem(R.id.im_completed, R.string.menu_uncompleted_full, R.drawable.ic_completed_off)
+			else -> MenuItem(R.id.im_completed, R.string.menu_completed_full, R.drawable.ic_completed_on)
 		}
 	}
 }
