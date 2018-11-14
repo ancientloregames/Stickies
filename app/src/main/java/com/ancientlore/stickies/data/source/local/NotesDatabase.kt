f84e6aff838d94180c795889a62f543871d655d9
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-11-14 19:43:54 +0200

    provided a migration scheme v2 to the db

--- app/src/main/java/com/ancientlore/stickies/data/source/local/NotesDatabase.kt
+++ app/src/main/java/com/ancientlore/stickies/data/source/local/NotesDatabase.kt
@@ -1,14 +1,17 @@
 package com.ancientlore.stickies.data.source.local
 
+import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
+import android.arch.persistence.room.migration.Migration
 import android.content.Context
 import com.ancientlore.stickies.data.model.Note
 import com.ancientlore.stickies.SingletonHolder
 import com.ancientlore.stickies.data.model.Topic
+import com.ancientlore.stickies.data.source.local.NotesDatabase.Companion.MIGRATION_1_2
 
-@Database(entities = [Note::class, Topic::class], version = 1)
+@Database(entities = [Note::class, Topic::class], version = 2)
 abstract class NotesDatabase : RoomDatabase() {
 
 	abstract fun notesDao(): NotesDao
@@ -16,6 +19,14 @@ abstract class NotesDatabase : RoomDatabase() {
 	abstract fun topicsDao(): TopicsDao
 
 	companion object : SingletonHolder<NotesDatabase, Context>({
-		Room.databaseBuilder(it, NotesDatabase::class.java, "notes.db").build()
-	})
+		Room.databaseBuilder(it, NotesDatabase::class.java, "notes.db")
+				.addMigrations(MIGRATION_1_2)
+				.build()
+	}) {
+		@JvmField val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+			override fun migrate(database: SupportSQLiteDatabase) {
+				database.execSQL("CREATE TABLE topics(name TEXT PRIMARY KEY NOT NULL);")
+			}
+		}
+	}
 }
