b30d71f1b331e299217d0d1cef50e0a20663efc7
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-11-16 20:22:29 +0200

    made the topic model parcelable

--- app/src/main/java/com/ancientlore/stickies/data/model/Topic.kt
+++ app/src/main/java/com/ancientlore/stickies/data/model/Topic.kt
@@ -2,6 +2,23 @@ package com.ancientlore.stickies.data.model
 
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import android.os.Parcel
+import android.os.Parcelable
 
 @Entity(tableName = "topics")
-data class Topic(@field:PrimaryKey var name: String)
+data class Topic(@field:PrimaryKey var name: String) : Parcelable {
+
+	companion object CREATOR : Parcelable.Creator<Topic> {
+		override fun createFromParcel(parcel: Parcel) = Topic(parcel)
+
+		override fun newArray(size: Int) = arrayOfNulls<Topic>(size)
+	}
+
+	constructor(parcel: Parcel) : this(parcel.readString())
+
+	override fun writeToParcel(parcel: Parcel, flags: Int) {
+		parcel.writeString(name)
+	}
+
+	override fun describeContents() = 0
+}
