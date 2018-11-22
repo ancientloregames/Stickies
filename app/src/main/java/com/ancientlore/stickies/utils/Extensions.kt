834cb85e4980ee55d7f16c3f5e5328b078bcf6a5
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-11-22 18:37:57 +0200

    added extensions for the convenient external apps launching

--- app/src/main/java/com/ancientlore/stickies/utils/Extensions.kt
+++ app/src/main/java/com/ancientlore/stickies/utils/Extensions.kt
@@ -1,6 +1,9 @@
 package com.ancientlore.stickies.utils
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.text.Html
 import android.view.View
 import android.view.inputmethod.InputMethodManager
@@ -14,5 +17,15 @@ fun Context.hideKeyboard(view: View) = hideKeyboard(view, 0)
 
 fun Context.hideKeyboard(view: View, flags: Int) =
 		(getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(view.windowToken, flags)
+
+fun Activity.tryStartActivity(intent: Intent, requestCode: Int): Boolean {
+	val activityExists = intent.isResolvable(packageManager)
+
+	if (activityExists) startActivityForResult(intent, requestCode)
+
+	return activityExists
+}
+
+fun Intent.isResolvable(packageManager: PackageManager) = resolveActivity(packageManager) != null
 
 fun String.toPlainText() = Html.fromHtml(this).toString()
